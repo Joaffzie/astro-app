9786fb3b76c441608b8909f2f96bed739753f1e0
yoav <joaffzie@gmail.com>
2021-11-15 11:39:00 +0200

    added files

--- app/app.py
+++ app/app.py
@@ -1,11 +1,24 @@
-from flask import Flask, Response, request, render_template
+import flask
+from flask import render_template, Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+mongodb_client = PyMongo(app, uri="mongodb://mongo:27017/db")
+db = mongodb_client.db
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route("/month")
 def month():
     pass
+
+@app.route("/name", methods=['POST', 'GET'])
+def name():
+    db.db.insert_one({'name':'yoav', 'age':'30'})
+    return flask.jsonify(message='success')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
